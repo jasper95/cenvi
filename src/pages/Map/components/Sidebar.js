baf0ef594c31992ecf2b069c9a4460771b55bfac
Jose Adrian Buctuanon <joseadrianbuctuanon@192.168.254.105>
2019-11-24 21:34:11 +0800

    map changes

--- src/pages/Map/components/Sidebar.js
+++ src/pages/Map/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button } from 'react-md';
 import cn from 'classnames'
 
@@ -6,67 +6,79 @@ import 'sass/components/mapSidebar/index';
 
 function Sidebar(props) {
   const { categories } = props 
+  const [sidebarOpen, setSidebarOpen] = useState(false)
   return (
-    <div className='sidebar'>
-        <div className='sidebar_header'>
-          <h1 className='sidebar_header_title'>
-            Map Viewer
-          </h1>
+    <div className={cn('sidebar', {
+      'sidebar-open': sidebarOpen
+    })}>
+      <Button
+        icon
+        iconClassName={cn('',{
+          'wtfs wtf-chevron-left': sidebarOpen,
+          'wtfs wtf-chevron-right': !sidebarOpen,
+        })}
+        className="sidebar_toggler"
+        onClick={() => setSidebarOpen(!sidebarOpen)}
+      />
+      <div className='sidebar_header'>
+        <h1 className='sidebar_header_title'>
+          Map Viewer
+        </h1>
+      </div>
+      <div className='sidebar_body'>
+        <div className='row'>
+          <TextField
+            id='name'
+            label='Available Layers'
+            className='iField'
+            placeholder='Search For ..'
+          />
         </div>
-        <div className='sidebar_body'>
-          <div className='row'>
-            <TextField
-              id='name'
-              label='Available Layers'
-              className='iField'
-              placeholder='Search For ..'
-            />
-          </div>
-          <div className='row row-filterCategories'>
-            <div className="col-12-guttered">
-              <label className="iField_label">
-                Filter Categories By:
-              </label>  
-              <div className="categories">
-                { categories && categories.map((cat) => (
-                  <Button
-                    children={cat.label}
-                    tooltipLabel={cat.label}
-                    tooltipPosition="top"
-                    className={cn('iBttn', {
-                      'iBttn-primary': cat.active,
-                      'iBttn-second-prio': !cat.active
-                    })}
-                  />
-                ))}
-              </div>
+        <div className='row row-filterCategories'>
+          <div className="col-12-guttered">
+            <label className="iField_label">
+              Filter Categories By:
+            </label>  
+            <div className="categories">
+              { categories && categories.map((cat) => (
+                <Button
+                  children={cat.label}
+                  tooltipLabel={cat.label}
+                  tooltipPosition="top"
+                  className={cn('iBttn', {
+                    'iBttn-primary': cat.active,
+                    'iBttn-second-prio': !cat.active
+                  })}
+                />
+              ))}
             </div>
           </div>
-          <div className="row">
-            <TextField
-              id='name'
-              label='Title'
-              className='iField'
-              rows={5}
-            />
-          </div>
-          <div className="row">
-            <TextField
-              id='loaded_layer'
-              label='Loaded Layers'
-              className='iField'
-              rows={5}
-            />
-          </div>
-          <div className="row">
-            <TextField
-              id='legend'
-              label='Legend'
-              className='iField'
-              rows={5}
-            />
-          </div>
         </div>
+        <div className="row">
+          <TextField
+            id='name'
+            label='Title'
+            className='iField'
+            rows={5}
+          />
+        </div>
+        <div className="row">
+          <TextField
+            id='loaded_layer'
+            label='Loaded Layers'
+            className='iField'
+            rows={5}
+          />
+        </div>
+        <div className="row">
+          <TextField
+            id='legend'
+            label='Legend'
+            className='iField'
+            rows={5}
+          />
+        </div>
+      </div>
     </div>
   )
 }
