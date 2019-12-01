9cdac25d486b330b49c955b86c85754df5dd9be5
Jab <iamthegoodshit95@gmail.com>
2019-12-02 01:01:46 +0800

    one level pie chart

--- src/pages/Admin/Dashboard/containers/Dashboard.js
+++ src/pages/Admin/Dashboard/containers/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CustomLineChart from 'shared/components/Charts/LineChart';
 import CustomBarChart from 'shared/components/Charts/BarChart';
+import CustomAreaChart from 'shared/components/Charts/AreaChart';
+import OneLevelPieChart from 'shared/components/Charts/OneLevelPieChart';
 import Paper from 'react-md/lib/Papers/Paper';
 
 function Dashboard() {
@@ -11,18 +13,47 @@ function Dashboard() {
           Hi Sample User
         </h1>
       </div>
+      <div className="row row-pies">
+        <Paper 
+          className="col col-md-4-guttered col-form"
+          style={{height: 400}}
+        >
+          <OneLevelPieChart />
+        </Paper>
+        <Paper 
+          className="col col-md-4-guttered col-form"
+          style={{height: 400}}
+        >
+          <OneLevelPieChart />
+        </Paper>
+        <Paper 
+          className="col col-md-4-guttered col-form"
+          style={{height: 400}}
+        >
+          <OneLevelPieChart />
+        </Paper>
+      </div>
       <div className="row row-content">
         <Paper 
           className="col col-md-8-guttered col-form"
-          style={{height: 500}}
+          style={{height: 250}}
         >
           <CustomLineChart />
         </Paper>
         <Paper 
           className="col col-md-4-guttered col-form"
-          style={{height: 500}}
+          style={{height: 250}}
         >
           <CustomBarChart />
+        </Paper>
+      </div>
+
+      <div className="row row-content-2">
+        <Paper 
+          className="col col-md-12-guttered col-form"
+          style={{height: 500}}
+        >
+          <CustomAreaChart />
         </Paper>
       </div>
     </>
