c2a636c4ea2f68ab12ebe6b47ac355714c0daa5c
Jab <iamthegoodshit95@gmail.com>
2019-12-02 00:29:46 +0800

    added bar chart

--- src/pages/Admin/Dashboard/containers/Dashboard.js
+++ src/pages/Admin/Dashboard/containers/Dashboard.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import CustomLineChart from 'shared/components/Charts/LineChart';
+import CustomBarChart from 'shared/components/Charts/BarChart';
+import Paper from 'react-md/lib/Papers/Paper';
 
 function Dashboard() {
-
   return (
-    <div>
-      <h1>DASHBOARD !</h1>
-      <div style={{width: '70%', height: 500 }}>
-        <CustomLineChart />
+    <>
+      <div className="row row-header dashboardHeader">
+        <h1 className="dashboardHeader_title">
+          Hi Sample User
+        </h1>
       </div>
-    </div>
+      <div className="row row-content">
+        <Paper 
+          className="col col-md-8-guttered col-form"
+          style={{height: 500}}
+        >
+          <CustomLineChart />
+        </Paper>
+        <Paper 
+          className="col col-md-4-guttered col-form"
+          style={{height: 500}}
+        >
+          <CustomBarChart />
+        </Paper>
+      </div>
+    </>
   );
 }
 
