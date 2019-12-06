e2db1e1438b6e0eac8833b3e8a67bb1275db9bc6
Jab <iamthegoodshit95@gmail.com>
2019-12-06 23:27:23 +0800

    updated single upload

--- src/shared/components/FileUpload/SingleFileUpload.js
+++ src/shared/components/FileUpload/SingleFileUpload.js
@@ -3,21 +3,33 @@ import { useDropzone } from 'react-dropzone';
 import cn from 'classnames';
 import ImageLoader from 'react-image';
 import TextFieldMessage from 'react-md/lib/TextFields/TextFieldMessage';
+import { useDispatch } from 'react-redux';
+import { showError } from 'shared/redux/app/reducer';
+import FILE_TYPE_IMAGES from 'shared/constants/fileType';
 
 function SingleFileUpload(props) {
   const {
     value, onChange, id, error,
+    acceptedFileTypes
   } = props;
   const [file, setFile] = useState(null);
   const [preview, setPreview] = useState(value);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     if (value && typeof value === 'string') {
       setPreview(value);
     }
   }, [value]);
-  const { getRootProps, getInputProps } = useDropzone({ onDrop, multiple: false });
+ 
+  const { getRootProps, getInputProps } = useDropzone({ 
+    onDrop, 
+    multiple: false,
+  }); 
+
 
   const hasPreviewImage = (file && file.type.includes('image')) || preview;
+
   return (
     <div
       className={cn('iDropzone iDropzone-singleUpload', { 'iDropzone-hasFiles': hasPreviewImage })}
@@ -29,7 +41,7 @@ function SingleFileUpload(props) {
       </p>
       { hasPreviewImage && (
         <div className="iDropzone_files">
-          <div className="iDropzone_file">
+          <div className={`iDropzone_file  iDropzone_file--${file.type.split('/')[1]}`}>
             <div className="iDropzone_file_preview">
               <ImageLoader
                 className="iDropzone_file_preview_src"
@@ -50,10 +62,29 @@ function SingleFileUpload(props) {
 
   function onDrop([acceptedFile]) {
     const reader = new FileReader();
-    reader.onload = () => {
-      setPreview(reader.result);
-    };
+    const fileType = acceptedFile.type.split('/')[1]
+
     setFile(acceptedFile);
+    
+    console.log('acceptedFile ==== ', acceptedFile)
+
+    if (acceptedFileTypes && !acceptedFileTypes.includes(fileType)) {
+      dispatch(showError({ message: 'File is not supported' }))
+      return;
+    }
+
+    const retrieveDefaultFileType = FILE_TYPE_IMAGES.find((f) => f.type === fileType)
+
+    console.log('retrieveDefaultFileType === ', retrieveDefaultFileType)
+
+    reader.onload = () => {
+      let preview = retrieveDefaultFileType 
+        ? retrieveDefaultFileType.image
+        : reader.result
+
+      setPreview(preview);
+    };
+
     onChange(acceptedFile, id);
     reader.readAsDataURL(acceptedFile);
   }
---
+++ src/shared/constants/fileType.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    type: 'zip',
+    image: '/static/img/file_icons/file_zip.png'
+  },
+  {
+    type: 'shp',
+    image: '/static/img/file_icons/file_shp.png'
+  },
+  {
+    type: 'kml',
+    image: '/static/img/file_icons/file_kml.png'
+  }
+]
