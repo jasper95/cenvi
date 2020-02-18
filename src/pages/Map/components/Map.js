5e38e7f52d7e9d348217128c798bad1ff744ce1a
Jab <iamthegoodshit95@gmail.com>
2020-02-18 21:35:46 +0800

    updated map resize

--- src/pages/Map/components/Map.js
+++ src/pages/Map/components/Map.js
@@ -5,31 +5,38 @@ import {
   WMSTileLayer,
 } from 'react-leaflet';
 import chunk from 'lodash/chunk';
-
+import ReactResizeDetector from 'react-resize-detector';
 
 export default function Map(props) {
   const { activeLayers, bbox } = props;
   return (
-    <LeafletMap
-      center={[10.9, 123.9]}
-      zoom={10}
-      {...bbox && {
-        bounds: chunk(bbox.slice().reverse(), 2),
-      }}
-    >
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {activeLayers.map(e => (
-        <WMSTileLayer
-          url={`${process.env.GEOSERVER_URL}/wms`}
-          layers={`cenvi:${e}`}
-          transparent
-          format="image/png"
-          srs={4326}
-        />
-      ))}
-    </LeafletMap>
+    <div className="map-container">
+      <ReactResizeDetector handleWidth handleHeight>
+        {({ width, height }) => (
+          <LeafletMap
+            style={{ height, width }}
+            center={[10.9, 123.9]}
+            zoom={10}
+            {...bbox && {
+              bounds: chunk(bbox.slice().reverse(), 2),
+            }}
+          >
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {activeLayers.map(e => (
+              <WMSTileLayer
+                url={`${process.env.GEOSERVER_URL}/wms`}
+                layers={`cenvi:${e}`}
+                transparent
+                format="image/png"
+                srs={4326}
+              />
+            ))}
+          </LeafletMap>
+        )}
+      </ReactResizeDetector>
+    </div>
   );
 }
