0e4cb3384c1fb57cc5cb6b0080d2a69dd6efaae5
Jasper Bernales <bernalesjasper@gmail.com>
2020-03-11 14:41:27 +0800

    PORT VARIABLES

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${PORT} ${API_PORT} ${GEOSERVER_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
