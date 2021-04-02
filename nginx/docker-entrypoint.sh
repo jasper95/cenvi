5f4cde64edf5421a6d66d1a5f64a458078f72014
Jasper Bernales <bernalesjasper@gmail.com>
2021-04-02 16:37:13 +0800

    env

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-envsubst '${PORT} ${API_PORT} ${GEOSERVER_PORT} ${SERVER_NAME}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${PORT} ${API_PORT} ${GEOSERVER_PORT} ${SERVER_NAME} ${GEOSERVER_API}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
